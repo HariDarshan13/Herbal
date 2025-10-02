a07e4f093b03458a80a0c9f67bb37123f73ea90b
HariDarshan13 <haridarshanhari123@gmail.com>
2025-10-02 11:12:34 +0530

    Update Contact.tsx

--- frontend/src/pages/feedback/Contact.tsx
+++ frontend/src/pages/feedback/Contact.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
     setIsLoading(true);
 
     try {
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch('https://herbal-backend-un9h.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
